b55c81ab8f81234d5e1ca613e110d8192e1ca417
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-11-30 08:40:31 -0700

    minor

--- test_get.js
+++ test_get.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '30s',
+  vus: 2000,
+  duration: '60s',
 };
 
 export default function () {
